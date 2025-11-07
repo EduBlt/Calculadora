0bc464dd599f661deeca4d8c969e5391473e003c
EduBlt <eduardobeltranclases@gmail.com>
2025-11-07 14:55:21 -0500

    Enhance program to support addition and subtraction
    Added functionality for subtraction and user operation selection.

--- SumaProgram.vb
+++ SumaProgram.vb
@@ -1,15 +1,32 @@
 Module Program
     Sub Main()
-        Dim numero1 As Integer
-        Dim numero2 As Integer 
-        Dim suma As Integer
+        Dim numero1 As Double
+        Dim numero2 As Double
+        Dim suma As Double
+        Dim resta As Double
+        Dim opcion As Integer
 
+        Console.WriteLine("¿Qué operación desea realizar?")
+        Console.WriteLine("1: Suma")
+        Console.WriteLine("2: Resta")
+        opcion = Convert.ToInt32(Console.ReadLine())
+        
         Console.WriteLine("Ingrese el primer número:")
         numero1 = Convert.ToDouble(Console.ReadLine())
-
         Console.WriteLine("Ingrese el segundo número:")
         numero2 = Convert.ToDouble(Console.ReadLine())
-        suma = numero1 + numero2
-        Console.WriteLine("La suma es: " & suma)
+
+        If opcion = 1 Then
+            suma = numero1 + numero2
+            Console.WriteLine("La suma es: " & suma)
+        Elseif opcion = 2 Then
+            resta = numero1 - numero2
+            Console.WriteLine("La resta es: " & resta)
+        Else
+            Console.WriteLine("Opción no válida")
+        End If
+        
+        Console.ReadKey()
+        
     End Sub
 End Module
