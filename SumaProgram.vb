680b87321129172de75287fe4f7ece3f18ce9ee6
kindaweird72 <vicpb27@gmail.com>
2025-11-08 14:40:25 -0500

    Add multiplication functionality to SumaProgram
    hecho por victoria

--- SumaProgram.vb
+++ SumaProgram.vb
@@ -3,6 +3,7 @@ Module Program
         Dim numero1 As Integer
         Dim numero2 As Integer 
         Dim suma As Integer
+        Dim multiplicacion As Double
 
         Console.WriteLine("Ingrese el primer número:")
         numero1 = Convert.ToDouble(Console.ReadLine())
@@ -10,6 +11,9 @@ Module Program
         Console.WriteLine("Ingrese el segundo número:")
         numero2 = Convert.ToDouble(Console.ReadLine())
         suma = numero1 + numero2
+        multiplicacion = numero1 * numero2
         Console.WriteLine("La suma es: " & suma)
+        Console.WriteLine("La multiplicación es: " & multiplicacion)
     End Sub
+
 End Module
